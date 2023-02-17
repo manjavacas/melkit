df23df35a93e5768aefaeca0424bc8d12bc34edc
Antonio <antomanjavacas@gmail.com>
2023-02-17 10:02:32 +0100

    Minor changes on documentation and README

--- docs/source/code/edition.py
+++ docs/source/code/edition.py
@@ -10,4 +10,16 @@ EDF_FILE = './edf_file.inp'
 
 toolkit = Toolkit(INPUT_FILE)
 
-toolkit.plot_edf(EDF_FILE)
+cv = toolkit.get_cv('CV001')
+
+# Edit CV field
+cv.update_field('NAME', 'my_volume')
+
+# Update in file
+toolkit.update_object(cv)
+
+# You can also edit several objects at the same time!
+other_cv = toolkit.get_cv('CV002')
+other_cv.update_field('ALTITUDE', 7.5)
+
+toolkit.update_objects([cv, other_cv])
