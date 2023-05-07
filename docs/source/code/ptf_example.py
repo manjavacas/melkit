7693460c5404eb394f5e13a6769abb200dcc1cc7
Matěj Rzehulka <matej.rzehulka@fykos.cz>
2023-05-07 15:16:50 +0200

    ptf examples

---
+++ docs/source/code/ptf_example.py
@@ -0,0 +1,27 @@
+'''
+Loading PTF files
+'''
+
+from melkit.ptf import Ptf, compare_ptf
+
+PATH_A = 'calcA/MELPTF'
+PATH_B = 'calcB/MELPTF'
+PATH_C = 'calcC/MELPTF'
+
+# create ptf objects
+ptf_a = Ptf(PATH_A)
+ptf_b = Ptf(PATH_B)
+ptf_c = Ptf(PATH_C)
+
+# list of columns in PTF file
+cols = ptf_a.columns
+
+# load subset of columns (first 5) and convert it to pandas DataFrame
+ptf_a.to_DataFrame([cols[:5]])
+
+# compare the first 2 varibles between files
+# only show plot
+compare_ptf([ptf_a, ptf_b, ptf_c], ["RN2-DFBBT-10-cls_7", "RN2-DFBBT-10-cls_8"])
+
+# compare, show plot and save them as png
+compare_ptf([ptf_a, ptf_b, ptf_c], ["RN2-DFBBT-10-cls_7", "RN2-DFBBT-10-cls_8"], save_dir="out")
